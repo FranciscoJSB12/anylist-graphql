b65561c7ac1e3044d605466d987ba0c4d38e526f
FranciscoJSB12 <javif8@gmail.com>
2024-06-06 19:45:59 -0400

    feat: create item entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
+      ConfigModule.forRoot(),
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        autoLoadEntities: true,
+        synchronize: true
+      }),
       GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: false,
--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,7 +1,29 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID, Float } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'items' })
+//El decorador entity no viene por defecto, pero es necesario aplicarlo
 @ObjectType()
+//Un dato muy importante es que el nombre que se le da a clase, el cual es Item en este caso viene siendo el nombre que tendrá en la base de datos
 export class Item {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  //El decorador field es lo que le dice a graphql que el objeto va a tener una propiedad llamada id, por ejemplo.
+  //@Field(() => Int, { description: 'Example field (placeholder)' })
+
+  @PrimaryGeneratedColumn('uuid') //PrimaryGeneratedColumn es para postgres, el tipado que necesita postgres
+  @Field(() => ID) // Field es para graphql que necesita un tipo específicado
+  id: string;
+
+  @Column()
+  @Field(() => String)
+  name: string;
+
+  @Column()
+  @Field(() => Float)
+  quantity: number;
+
+  @Column()
+  @Field(() => String)
+  quantityUnits: string;
 }
+
+//IMPORTANTE: luego de declara la entidad, que es cómo será la tabla en la base de datos, vamos al module (items.module) en este caso y lo importamos como se hizo, con esto ya se establece la tabla
